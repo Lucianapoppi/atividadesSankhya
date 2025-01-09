9944e2b70446cf9551e78a880a6f8345438281d1
Lucianapoppi <80785603+Lucianapoppi@users.noreply.github.com>
2025-01-09 06:24:52 -0300

    Finalizando desafio2Aperfeicoado

--- treinandoJava/src/desafio2Aperfeicoado/Principal.java
+++ treinandoJava/src/desafio2Aperfeicoado/Principal.java
@@ -1,102 +1,107 @@
 package desafio2Aperfeicoado;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Principal {
 
-	
-		public static void main(String[] args) {
-			
-			// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
 
-			/*
-			 * Desafio: Cadastro de turmas e estudantes
-			 * 
-			 * 1. No projeto de gestao escolar, apague todo o codigo existente ou comente-o;
-			 * 2. Escrever um algoritmo para exibir um menu com as opcoes: a) Cadastrar
-			 * turma; b) Cadastrar estudante; c) Listar turmas; d) Listar estudantes; e)
-			 * sair.
-			 * 
-			 * 3. Ler do teclado a opcao escolhida; 4. Para cada opcao escolhida,
-			 * implementar seu respectivo algoritmo e, no final, reexibir o menu; 5.
-			 * 
-			 * 
-			 * OBSERVACAO: 1. Ao cadastrar turma, solicitar: a) Codigo; b) Nome; c)
-			 * Capacidade.
-			 * 
-			 * 2. Ao cadastrar estudante, solicitar: a) Nome; b) Telefone; c) Endereço; d)
-			 * Responsavel.
-			 * 
-			 * 
-			 */
+		// TODO Auto-generated method stub
 
-			var leitor = new Scanner(System.in);
-			var opcaoDigitada = 0;
-			
-			var estudantes = new ArrayList<Estudante>();
-			var turmas = new ArrayList<Turma>();
+		/*
+		 * Desafio: Cadastro de turmas e estudantes
+		 * 
+		 * 1. No projeto de gestao escolar, apague todo o codigo existente ou comente-o;
+		 * 2. Escrever um algoritmo para exibir um menu com as opcoes: a) Cadastrar
+		 * turma; b) Cadastrar estudante; c) Listar turmas; d) Listar estudantes; e)
+		 * sair.
+		 * 
+		 * 3. Ler do teclado a opcao escolhida; 4. Para cada opcao escolhida,
+		 * implementar seu respectivo algoritmo e, no final, reexibir o menu; 5.
+		 * 
+		 * 
+		 * OBSERVACAO: 1. Ao cadastrar turma, solicitar: a) Codigo; b) Nome; c)
+		 * Capacidade.
+		 * 
+		 * 2. Ao cadastrar estudante, solicitar: a) Nome; b) Telefone; c) Endereço; d)
+		 * Responsavel.
+		 * 
+		 * 
+		 */
 
-			boolean continuar = true;
-			while (opcaoDigitada != 5) {
+		Scanner leitor = new Scanner(System.in);
+		var opcaoDigitada = 0;
 
-				System.out.println("1 - Cadastrar turma");
-				System.out.println("2 - Cadastrar estudante");
-				System.out.println("3 - Listar turmas");
-				System.out.println("4 - Listar estudantes");
-				System.out.println("5 - sair");
-				System.out.println("Digite o código da opção:");
-				
-				opcaoDigitada = Integer.parseInt(leitor.nextLine());
-				if (opcaoDigitada == 1) {
-					cadastrarTurma(leitor, turmas);
-					
-					
+		var estudantes = Path.of("estudantes.csv");
+		if(!Files.exists(estudantes)) {
+			Files.createFile(estudantes);
+		}
+		var turmas = Path.of("turmas.csv");
+		if(!Files.exists(turmas)) {
+			Files.createFile(turmas);
+		}
 
-				} else if (opcaoDigitada == 2) {
-					cadastrarEstudante(leitor, estudantes);								
-										
-						
-				} else if (opcaoDigitada == 3) {
-					listarTurmas(turmas);					
-									
-									
+		boolean continuar = true;
+		while (opcaoDigitada != 5) {
 
-				} else if (opcaoDigitada == 4) {
-					listarEstudantes(estudantes);
-										
-					
+			System.out.println("1 - Cadastrar turma");
+			System.out.println("2 - Cadastrar estudante");
+			System.out.println("3 - Listar turmas");
+			System.out.println("4 - Listar estudantes");
+			System.out.println("5 - sair");
+			System.out.println("Digite o código da opção:");
 
-				} else if (opcaoDigitada == 5) {
-					System.out.println("Você escolheu sair.");
-					
-									
-				} else {
-					System.out.println("Opção inválida! Por favor, escolha uma opção válida.");
-				}
+			opcaoDigitada = Integer.parseInt(leitor.nextLine());
+			if (opcaoDigitada == 1) {
+				cadastrarTurma(leitor, turmas);
 
+			} else if (opcaoDigitada == 2) {
+				cadastrarEstudante(leitor, estudantes);
+
+			} else if (opcaoDigitada == 3) {
+				listarTurmas(turmas);
+
+			} else if (opcaoDigitada == 4) {
+				listarEstudantes(estudantes);
+
+			} else if (opcaoDigitada == 5) {
+				System.out.println("Você escolheu sair.");
+
+			} else {
+				System.out.println("Opção inválida! Por favor, escolha uma opção válida.");
 			}
-		}
 
-		private static void cadastrarTurma(Scanner leitor, ArrayList<Turma> turmas){
-			System.out.println("Você escolheu cadastrar uma turma.");
-			
-			System.out.println("Digite o codigo da turma:");									
-			var codigoTurma = leitor.nextLine();
-			
-			System.out.println("Digite o nome da turma:");
-			var nomeTurma = leitor.nextLine();
-			
-			System.out.println("Digite a capacidade da turma:");
-			var capacidadeTurma = Integer.parseInt(leitor.nextLine());
-								
-			var turma = new Turma(codigoTurma, nomeTurma, capacidadeTurma);									
-														
-			turmas.add(turma);
 		}
-				
-				
-		private static void	cadastrarEstudante(Scanner leitor, ArrayList<Estudante> estudantes){	
+	}
+
+	private static void cadastrarTurma(Scanner leitor, Path arquivo) throws IOException {
+		
+		System.out.println("Você escolheu cadastrar uma turma.");
+
+		System.out.println("Digite o codigo da turma:");
+		var codigoTurma = leitor.nextLine();
+
+		System.out.println("Digite o nome da turma:");
+		var nomeTurma = leitor.nextLine();
+
+		System.out.println("Digite a capacidade da turma:");
+		var capacidadeTurma = Integer.parseInt(leitor.nextLine());
+
+		var turma = new Turma(codigoTurma, nomeTurma, capacidadeTurma);
+
+		Files.writeString(
+				arquivo, 
+				turma.getCodigo() + "," + turma.getNome() + "," + turma.getCapacidade() +"\n",
+				StandardOpenOption.APPEND);
+		
+	}
+
+	private static void	cadastrarEstudante(Scanner leitor, Path arquivo) throws IOException{	
 		System.out.println("Você escolheu cadastrar um estudante.");
 		
 		System.out.println("Digite o nome do estudante:");
@@ -112,33 +117,32 @@ public class Principal {
 		String nomeResponsavel = leitor.nextLine();
 		
 		var estudante = new Estudante(nomeEstudante, nroTelefone, endEstudante, nomeResponsavel);
+		
+		Files.writeString(
+				arquivo, 
+				estudante.getNome() + "," + estudante.getTelefone() + "," + estudante.getEndereco() + "," + estudante.getResponsavel() +"\n", 
+				 
+				StandardOpenOption.APPEND);
+ 			
+		}
 
-		estudantes.add(estudante);
-			
-		}
-		
-		private static void	listarTurmas(ArrayList<Turma> turmas){
-			System.out.println("Você escolheu listar as turmas.");
-			
-			for(int i = 0; i < turmas.size(); i++) {
-				System.out.println("turma:  "+ turmas.get(i).getCodigo() + ", " + turmas.get(i).getNome() + ", " + turmas.get(i).getCapacidade());
+	private static void listarTurmas(Path arquivo) throws IOException{
+		var linhas = Files.readAllLines(arquivo);
 				
-			}
+		for(var linha : linhas) {
+			var campos = linha.split(",");
+			System.out.println(campos[0] + " - " + campos[1] + " - " + campos[2]);
 		}
-		
-		
-		private static void listarEstudantes(ArrayList<Estudante> estudantes){
+			
+	}
 
-			
-			System.out.println("Você escolheu listar os estudantes.");
-			
-			for(int i = 0; i<estudantes.size(); i++) {
-				System.out.println("estudantes:  "+ estudantes.get(i).getNome() + ", " + estudantes.get(i).getTelefone() + ", " + estudantes.get(i).getEndereco()+ ", " + estudantes.get(i).getResponsavel());
-				
-			}
-		}
+	private static void listarEstudantes(Path estudantes) throws IOException{
+		var linhas = Files.readAllLines(estudantes);
 		
+		for(var linha : linhas) {
+			var campos = linha.split(",");
+			System.out.println(campos[0] + " - " + campos[1] + " - " + campos[2]+ " - " + campos[3]);
+		}
+
+	}
 }
-	
-	
-
---
+++ treinandoJava/src/desafio2Aperfeicoado/TesteErros.java
@@ -0,0 +1,44 @@
+package desafio2Aperfeicoado;
+import java.util.Scanner;
+
+public class TesteErros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Boas vindas ao sistema!");
+		
+		var scanner = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro valor: ");
+		var valor1 = Integer.parseInt(scanner.nextLine());
+		
+		System.out.println("Digite o segundo valor: ");
+		var valor2 = Integer.parseInt(scanner.nextLine());
+		
+		try {
+		 var resultado = calcular(valor1, valor2);
+		
+		System.out.println("Resultado da divisão: " +resultado);
+		System.out.println("Saindo do programa!");
+		
+		}catch(Exception e) {
+			System.out.println("Cálculo falhou!");
+			
+		}
+				
+	}
+	
+
+
+
+
+	private static int dividir(int valor1, int valor2) {
+		System.out.println("Calculando divisão");
+		var resultado = valor1 / valor2;
+		System.out.println("divisão efetuada");
+		return resultado;		
+				
+	}
+
+}
